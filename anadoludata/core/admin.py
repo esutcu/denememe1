174de764ecf7f75bdbe7d2de8aace9c33fb82c6f
Erol Sutcu <debuggajavu@gmail.com>
2025-07-08 03:36:54 -0400

    temizleme

--- anadoludata/core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User, Taxonomy, Question, Test, UserCredit
-
-admin.site.register(User)
-admin.site.register(Taxonomy)
-admin.site.register(Question)
-admin.site.register(Test)
-admin.site.register(UserCredit)
